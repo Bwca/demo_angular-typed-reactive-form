a5985477f2ff160f64a69b981f29b14fa5f2dfd7
Yepishev Volodymyr <i.m.bwca@gmail.com>
2020-08-20 20:35:34 +0300

    Added Monolithic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {TypedFormGroup} from './models/typed-reactive-form/typed-form-group.model';
+import {Address, Department, EmployeeDto} from './models/employee.dto';
+import {TypedControlConfig} from './models/typed-reactive-form/typed-control-config.type';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'typed-reactive-form';
+export class AppComponent implements OnInit {
+
+  employeeFormGroupOne: TypedFormGroup<EmployeeDto>;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.initFormOne();
+  }
+
+  private initFormOne(): void {
+    const fbPayload: TypedControlConfig<EmployeeDto> = {
+      creationDate: ['creationDate'],
+      department: {
+        address: {
+          building: ['building'],
+          city: ['city'],
+          code: ['code'],
+          country: ['country'],
+          street: ['street']
+        },
+        name: ['name'],
+        phone: ['phone'],
+        updateDate: ['updateDate']
+      },
+      email: ['email'],
+      firstName: ['firstName'],
+      id: [1],
+      lastName: ['lastName'],
+      middleName: ['middleName'],
+      parentEmployee: {
+        email: ['email'],
+        firstName: ['firstName'],
+        lastName: ['lastName'],
+        middleName: ['middleName'],
+        phone: ['phone'],
+        position: {
+          actualFrom: ['actualFrom'],
+          actualTo: ['actualTo'],
+          name: ['name']
+        }
+      },
+      permissions: ['permissions'],
+      phone: ['phone'],
+      position: {
+        name: ['name'],
+        actualTo: ['actualTo'],
+        actualFrom: ['actualFrom']
+      }
+    };
+
+    this.employeeFormGroupOne = this.fb.group(fbPayload) as TypedFormGroup<EmployeeDto>;
+  }
 }
---
+++ src/app/models/typed-reactive-form/typed-form-group.model.ts
@@ -0,0 +1,15 @@
+import { AbstractControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+export interface TypedFormGroup<T> extends FormGroup {
+  value: Partial<T>;
+  valueChanges: Observable<T>;
+
+  setValue(val: T): void;
+
+  getRawValue(): T;
+
+  patchValue(val: Partial<T>): void;
+
+  get<K>(path: keyof T & string): TypedFormGroup<K>;
+}
