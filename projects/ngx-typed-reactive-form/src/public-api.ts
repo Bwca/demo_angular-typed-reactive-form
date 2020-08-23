41ee6514d5bc6d153f533c7cabe32d4efec12a7f
Yepishev Volodymyr <i.m.bwca@gmail.com>
2020-08-23 15:55:04 +0300

    moved typed stuff to separate library

--- projects/ngx-typed-reactive-form/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-typed-reactive-form
- */
-
-export * from './lib/models/typed-form-group.model';
-export * from './lib/models/typed-control-config.type';
-export * from './lib/typed-form-group-builder/typed-form-group-builder.service';
